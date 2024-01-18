39421b34789f6fb1a90a5c7e7a6feab8bc89c76f
gedesurya125 <gedesurya125@gmail.com>
2024-01-18 12:30:28 +0800

    Initial commit from create-refine-app

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import { notificationProvider, RefineThemes } from "@refinedev/chakra-ui";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import routerBindings, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import dataProvider from "@refinedev/simple-rest";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        {/* You can change the theme colors here. example: theme={RefineThemes.Magenta} */}
+        <ChakraProvider theme={RefineThemes.Blue}>
+          <DevtoolsProvider>
+            <Refine
+              notificationProvider={notificationProvider}
+              routerProvider={routerBindings}
+              dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                useNewQueryKeys: true,
+                projectId: "BWL7f6-B5c0Me-VMaOj2",
+              }}
+            >
+              <Routes>
+                <Route index element={<WelcomePage />} />
+              </Routes>
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+            <DevtoolsPanel />
+          </DevtoolsProvider>
+        </ChakraProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
